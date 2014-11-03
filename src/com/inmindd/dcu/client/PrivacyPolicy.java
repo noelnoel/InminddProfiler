4c83856a411c463c5eb4256a8e3f9b892ed4a68f
scrineym <mscriney@computing.dcu.ie>
2014-11-03 14:32:35 +0000

    adding in privacy policy page

---
+++ src/com/inmindd/dcu/client/PrivacyPolicy.java
@@ -0,0 +1,22 @@
+package com.inmindd.dcu.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class PrivacyPolicy implements EntryPoint
+{
+
+	@Override
+	public void onModuleLoad()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	private void globalise()
+	{
+		
+	}
+
+}
